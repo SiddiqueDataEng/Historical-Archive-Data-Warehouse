5b96aa7db8b4eaaa01e803b5b804026ce6070fff
Muhammad Siddique <siddique@dataeng.com>
2014-05-08 09:04:00 +0500

    Implement triggers and data validation

---
+++ database/05-SeedData.sql
@@ -0,0 +1,19 @@
+/*
+ * Historical Archive Data Warehouse
+ * Sample Data
+ * SQL Server 2008/2012
+ */
+
+USE ArchiveDW;
+GO
+
+-- Insert sample records
+INSERT INTO dbo.MainRecords (RecordCode, RecordName, Description, CreatedBy)
+VALUES
+('REC001', 'Sample Record 1', 'First sample record for Historical-Archive-Data-Warehouse', 'admin'),
+('REC002', 'Sample Record 2', 'Second sample record for Historical-Archive-Data-Warehouse', 'admin'),
+('REC003', 'Sample Record 3', 'Third sample record for Historical-Archive-Data-Warehouse', 'admin');
+
+PRINT 'Sample data inserted successfully';
+PRINT 'Total records: ' + CAST(@@ROWCOUNT AS VARCHAR(10));
+GO
